55dc0720ba785d8c7267964122894b8188503a37
Abhishek Narayana <abhishek.narayana.0912@gmail.com>
2022-03-30 13:38:16 +0530

    New problem added.

---
+++ CommonDigitInTwoArray/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommonDigitInTwoArray
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Given two sorted integer array find the common digit in it with best time complexity
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[] array2 = new int[] { 0, 10, 15, 30, 40, 50 };
+            int[] array1 = new int[] { 5, 15, 30, 50, 65, 120 };
+            List<int> commonDigit = GetCommonDigit(array1, array2);
+            commonDigit.ForEach(Console.WriteLine);
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Gets the common digit in two sorted array in O(n) times
+        /// </summary>
+        /// <param name="array_1"></param>
+        /// <param name="array_2"></param>
+        /// <returns></returns>
+        private static List<int> GetCommonDigit(int[] array_1, int[] array_2)
+        {
+            if (array_1.Length == 0 || array_2.Length == 0)
+                return null;
+
+            List<int> commonDigit = new List<int>();
+            int index_2 = 0;
+            for (int index_1 = 0; index_1 < array_1.Length && index_2 < array_2.Length; index_1++)
+            {
+                while(index_2 < array_2.Length)
+                {
+                    if (array_1[index_1] > array_2[index_2])
+                        index_2 += 1;
+                    else if (array_1[index_1] < array_2[index_2])
+                        break;
+                    else
+                    {
+                        commonDigit.Add(array_1[index_1]);
+                        index_2 += 1;
+                        break;
+                    }
+                }
+            }
+
+            return commonDigit;
+        }
+    }
+}
--- UniqueCharCount/Program.cs
+++ UniqueCharCount/Program.cs
@@ -6,13 +6,18 @@ namespace UniqueCharCount
 {
     class Program
     {
+        // Given a string, get the count of each char to be removed so that each char count is unique.
+        // Ex : aaaabbbb => removing 'b' once makes char count as a = 4, b = 3.
+        // Ex : aaaabbbbcccc => removing 'b' once and removing 'c' twice makes char count as a = 4, b = 3, c = 2.
+        // Ex : aaabbbcccddd => removing 'b' 1 time , removing 'c' 2 time and removing 'd' 3 times makes char count as a = 3, b = 2, c = 1.
+
         public Dictionary<char, int> charCollection = new Dictionary<char, int>();
         public Dictionary<char, int> removedCharCollection = new Dictionary<char, int>();
 
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.GetCharToBeRemovedToHaveUniqueCharCount("a aa bbb ccc");
+            program.UpdateCharCollectionToHaveUniqueCharCount("aaabbbccc");
 
             foreach (KeyValuePair<char, int> item in program.removedCharCollection)
             {
@@ -21,7 +26,7 @@ namespace UniqueCharCount
             Console.ReadLine();
         }
 
-        private void GetCharToBeRemovedToHaveUniqueCharCount(string inputString)
+        private void UpdateCharCollectionToHaveUniqueCharCount(string inputString)
         {
             UpdateCharCount(inputString);
             RemoveDuplicateCharFromCollection();
