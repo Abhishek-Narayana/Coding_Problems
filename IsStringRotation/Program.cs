5a130849c3860f6bf626847c2bf56f3768c31616
Abhishek Narayana <abhishek.narayana.0912@gmail.com>
2022-05-21 08:46:08 +0530

    Improved IsRotationString program

--- IsStringRotation/Program.cs
+++ IsStringRotation/Program.cs
@@ -17,12 +17,7 @@ namespace IsStringRotation
             if (string.IsNullOrEmpty(s1) || string.IsNullOrEmpty(s2))
                 return false;
 
-            return IsSubString(s1 + s1, s2);
-        }
-
-        private static bool IsSubString(string s1s1, string s2)
-        {
-            return s1s1.Contains(s2);
+            return (s1 + s1).Contains(s2);
         }
     }
 }
