12b81b2bb8962d945ccc8751563c37b5123e2488
Abhishek Narayana <abhishek.narayana.0912@gmail.com>
2022-05-25 08:21:02 +0530

    * MinimumCharNeedToBeInsertedToMakeItPalindrome * Push all zero to the end

---
+++ MinimumCharNeedToBeInsertedToMakeItPalindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MinimumCharNeedToBeInsertedToMakeItPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = MinimumCharNeedToBeInsertedToMakeItPalindrome("abcba");
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+
+        private static int MinimumCharNeedToBeInsertedToMakeItPalindrome(string s)
+        {
+            int Count = 0;
+            for (int start = 0, end = s.Length - 1; start < end; start++)
+            {
+                if (s[start] != s[end])
+                {
+                    Count++;
+                    continue;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+            return Count;
+        }
+    }
+}
---
+++ PushAllZeroToTheEnd/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PushAllZeroToTheEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> number = new List<int>() {2, 0, 1, 4, 0, 5, 6, 1 , 0 };
+            PushAllZeroToTheEnd(ref number);
+            foreach (var item in number)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        private static void PushAllZeroToTheEnd(ref List<int> number)
+        {
+            int ZeroCount = 0;
+            for (int index = 0; index < number.Count; index++)
+            {
+                if(number[index] == 0 )
+                {
+                    number.RemoveAt(index);
+                    ZeroCount++;
+                }
+            }
+            for (int i = 0; i < ZeroCount; i++)
+            {
+                number.Add(0);
+            }
+        }
+    }
+}
